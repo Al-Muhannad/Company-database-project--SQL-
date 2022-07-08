b97700b86a39359f6d34e19a2daa16fca305138e
mj <almohand2011@gmail.com>
2022-07-08 18:59:34 +0300

    first commit

---
+++ Company database project (SQL).sql
@@ -0,0 +1,201 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for macos11 (x86_64)
+--
+-- Host: localhost    Database: company
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `department`
+--
+
+DROP TABLE IF EXISTS `department`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `department` (
+  `Dname` varchar(15) NOT NULL,
+  `dno` int NOT NULL,
+  `Mgr_ssn` char(9) NOT NULL,
+  `Mgr_start_date` date DEFAULT NULL,
+  PRIMARY KEY (`dno`),
+  UNIQUE KEY `Dname` (`Dname`),
+  KEY `Mgr_ssn` (`Mgr_ssn`),
+  CONSTRAINT `department_ibfk_1` FOREIGN KEY (`Mgr_ssn`) REFERENCES `EMPLOYEE` (`Ssn`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `department`
+--
+
+LOCK TABLES `department` WRITE;
+/*!40000 ALTER TABLE `department` DISABLE KEYS */;
+INSERT INTO `department` VALUES ('Headquarters',1,'888665555','2011-01-09'),('Administration',4,'987654321','2016-11-22'),('Research',5,'333445555','2013-03-01'),('Automation',7,'123456789','2020-07-04');
+/*!40000 ALTER TABLE `department` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `dependent`
+--
+
+DROP TABLE IF EXISTS `dependent`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `dependent` (
+  `Essn` char(9) NOT NULL,
+  `Dependent_name` varchar(15) NOT NULL,
+  `Sex` char(1) DEFAULT NULL,
+  `Bdate` date DEFAULT NULL,
+  `Relationship` varchar(8) DEFAULT NULL,
+  PRIMARY KEY (`Essn`,`Dependent_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dependent`
+--
+
+LOCK TABLES `dependent` WRITE;
+/*!40000 ALTER TABLE `dependent` DISABLE KEYS */;
+INSERT INTO `dependent` VALUES ('123456789','Alice','F','2018-05-15','Daughter'),('123456789','Elizabeth','F','1999-02-08','Spouse'),('123456789','Michael','M','2019-08-25','Son'),('333445555','Alice','F','2012-06-05','Daughter'),('333445555','Joy','F','1997-08-18','Spouse'),('333445555','Theodore','M','2015-03-29','Son'),('987654321','Abner','M','1998-04-01','Spouse');
+/*!40000 ALTER TABLE `dependent` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Dept_Locations`
+--
+
+DROP TABLE IF EXISTS `Dept_Locations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Dept_Locations` (
+  `Dnumber` int NOT NULL,
+  `Dlocation` varchar(15) NOT NULL,
+  PRIMARY KEY (`Dnumber`,`Dlocation`),
+  CONSTRAINT `dept_locations_ibfk_1` FOREIGN KEY (`Dnumber`) REFERENCES `DEPARTMENT` (`dno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Dept_Locations`
+--
+
+LOCK TABLES `Dept_Locations` WRITE;
+/*!40000 ALTER TABLE `Dept_Locations` DISABLE KEYS */;
+INSERT INTO `Dept_Locations` VALUES (1,'Houston'),(4,'Stafford'),(5,'Bellaire'),(5,'Houston'),(5,'Sugarland');
+/*!40000 ALTER TABLE `Dept_Locations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `employee`
+--
+
+DROP TABLE IF EXISTS `employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `employee` (
+  `Fname` varchar(15) NOT NULL,
+  `Minit` char(1) DEFAULT NULL,
+  `Lname` varchar(15) NOT NULL,
+  `Ssn` char(9) NOT NULL,
+  `Bdate` date DEFAULT NULL,
+  `Address` varchar(15) DEFAULT NULL,
+  `Sex` char(1) DEFAULT NULL,
+  `Salary` decimal(10,2) DEFAULT NULL,
+  `Super_ssn` char(9) DEFAULT NULL,
+  `Dno` int NOT NULL,
+  PRIMARY KEY (`Ssn`),
+  KEY `Super_ssn` (`Super_ssn`),
+  KEY `Dno` (`Dno`),
+  CONSTRAINT `employee_ibfk_1` FOREIGN KEY (`Super_ssn`) REFERENCES `employee` (`Ssn`),
+  CONSTRAINT `employee_ibfk_2` FOREIGN KEY (`Dno`) REFERENCES `DEPARTMENT` (`dno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employee`
+--
+
+LOCK TABLES `employee` WRITE;
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+INSERT INTO `employee` VALUES ('Joy','B','Smith','123456789','1955-01-09','731 Fondren','F',30000.00,'987654321',5),('Franklin','T','Wong','333445555','1990-12-25','638 Voss','M',40000.00,'888665555',5),('John','A','English','453453453','1991-01-30','3321 Castle','F',25000.00,'333445555',5),('John','K','Narayan','666884444','1992-03-11','5631 Rice','M',38000.00,'333445555',5),('John','E','Borg','888665555','1993-05-28','975 Fire Oak','M',55000.00,'987654321',1),('John','S','Wallace','987654321','1994-06-25','450 Stone','F',43000.00,'987654321',4),('John','V','Jabbar','987987987','1995-08-29','291 Berry','M',25000.00,'987654321',4),('John','J','Zelaya','999887777','1996-11-19','980 Dallas','F',25000.00,'987654321',4);
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `PROJECT`
+--
+
+DROP TABLE IF EXISTS `PROJECT`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `PROJECT` (
+  `Pname` varchar(15) NOT NULL,
+  `Pnumber` int NOT NULL,
+  `Plocation` varchar(15) DEFAULT NULL,
+  `Dnum` int NOT NULL,
+  PRIMARY KEY (`Pnumber`),
+  UNIQUE KEY `Pname` (`Pname`),
+  KEY `Dnum` (`Dnum`),
+  CONSTRAINT `project_ibfk_1` FOREIGN KEY (`Dnum`) REFERENCES `department` (`dno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `PROJECT`
+--
+
+LOCK TABLES `PROJECT` WRITE;
+/*!40000 ALTER TABLE `PROJECT` DISABLE KEYS */;
+INSERT INTO `PROJECT` VALUES ('ProductX',1,'Bellaire',5),('ProductY',2,'Sugarland',5),('ProductZ',3,'Houston',5),('Computerization',10,'Stafford',4),('Reorganization',20,'Houston',1),('Newbenefits',30,'Stafford',4),('Project xxx',69,'Khaldeyah',5);
+/*!40000 ALTER TABLE `PROJECT` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `works_on`
+--
+
+DROP TABLE IF EXISTS `works_on`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `works_on` (
+  `Essn` char(9) NOT NULL,
+  `Pno` int NOT NULL,
+  `Hours` decimal(3,1) NOT NULL,
+  PRIMARY KEY (`Essn`,`Pno`),
+  KEY `Pno` (`Pno`),
+  CONSTRAINT `works_on_ibfk_1` FOREIGN KEY (`Essn`) REFERENCES `employee` (`Ssn`),
+  CONSTRAINT `works_on_ibfk_2` FOREIGN KEY (`Pno`) REFERENCES `project` (`Pnumber`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `works_on`
+--
+
+LOCK TABLES `works_on` WRITE;
+/*!40000 ALTER TABLE `works_on` DISABLE KEYS */;
+INSERT INTO `works_on` VALUES ('123456789',1,32.5),('123456789',2,7.5),('333445555',2,10.0),('333445555',3,10.0),('333445555',10,10.0),('333445555',20,10.0),('453453453',1,20.0),('453453453',2,20.0),('666884444',3,40.0),('666884444',20,15.0),('888665555',20,40.0),('888665555',30,20.0),('987987987',10,35.0),('987987987',30,5.0),('999887777',10,10.0),('999887777',30,30.0);
+/*!40000 ALTER TABLE `works_on` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-07-08 15:14:42
